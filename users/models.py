9eb208c52a322f2314a78bc63ba420f7b67c877a
nikoloz samkharadze <samxaradzenikoloz8@gmail.com>
2026-01-26 19:00:43 +0400

    sdsad

--- users/models.py
+++ users/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_pics/', default='default.jpg')
-    bio = models.TextField(blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f"{self.user.username}'s profile"
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- users/views.py
+++ users/views.py
@@ -1,24 +1,42 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.contrib import messages
-from users.forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
+
 def register(request):
-    if request.method=='POST':
-        form=UserRegisterForm(request.POST)
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-         form.save()
-         username =form.cleaned_data.get('username')
-        messages.success(request,f'your account has been created for {username}!')
-        return redirect('login')
-           
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
-     form=UserRegisterForm()
-     return render(request,'users/register.html',{'form':form})
-    
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
-    
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
 
-# Create your views here.
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
